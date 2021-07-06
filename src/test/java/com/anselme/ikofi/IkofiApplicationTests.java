9e068c3d4c96a7c4243e68df95f63eebcf347a9f
irumvanselme <andesanselme@gmail.com>
2021-07-06 20:35:15 +0200

    Initial commit

---
+++ src/test/java/com/anselme/ikofi/IkofiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anselme.ikofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkofiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

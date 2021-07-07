b4ef5734df9c519935d3bfa48ca032b6f69ff3d9
irumvanselme <andesanselme@gmail.com>
2021-07-07 15:56:19 +0200

    Add sent and received transactions

---
+++ src/main/java/com/anselme/ikofi/controllers/AccountController.java
@@ -0,0 +1,43 @@
+package com.anselme.ikofi.controllers;
+
+import com.anselme.ikofi.models.User;
+import com.anselme.ikofi.services.AccountService;
+import com.anselme.ikofi.services.UserService;
+import com.anselme.ikofi.utils.dto.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+    private final UserService userService;
+
+    @Autowired
+    public AccountController(AccountService service, UserService userService) {
+        this.accountService = service;
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse> getAllTransactions() {
+        User user = userService.getLoggedInUser();
+        return ResponseEntity.ok(new ApiResponse(accountService.transactions(user.getAccount().getId())));
+    }
+
+    @GetMapping("/sent")
+    public ResponseEntity<ApiResponse> getAllTransactionsThatWhereSentByMe() {
+        User user = userService.getLoggedInUser();
+        return ResponseEntity.ok(new ApiResponse(user.getAccount().getSent()));
+    }
+
+    @GetMapping("/received")
+    public ResponseEntity<ApiResponse> getAllTransactionsThatWhereReceivedByMe() {
+        User user = userService.getLoggedInUser();
+        return ResponseEntity.ok(new ApiResponse(user.getAccount().getReceived()));
+    }
+}
--- src/main/java/com/anselme/ikofi/services/AccountService.java
+++ src/main/java/com/anselme/ikofi/services/AccountService.java
@@ -1,7 +1,23 @@
 package com.anselme.ikofi.services;
 
+import com.anselme.ikofi.models.Transaction;
+import com.anselme.ikofi.repositories.ITransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
+
+    private final ITransactionRepository transactionRepository;
+
+    @Autowired
+    public AccountService(ITransactionRepository repository){
+        this.transactionRepository = repository;
+    }
+
+    public List<Transaction> transactions(Long id){
+        return transactionRepository.getAllTransactionsByAccount(id);
+    }
 }

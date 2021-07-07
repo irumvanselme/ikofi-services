50c1dd7af0f92edfb02bfd0236142c8679458898
irumvanselme <andesanselme@gmail.com>
2021-07-07 15:46:36 +0200

    Add get all transactions

--- src/main/java/com/anselme/ikofi/repositories/ITransactionRepository.java
+++ src/main/java/com/anselme/ikofi/repositories/ITransactionRepository.java
@@ -2,8 +2,15 @@ package com.anselme.ikofi.repositories;
 
 import com.anselme.ikofi.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM transactions WHERE sender_id = :account_id OR receiver_id = :account_id")
+    List<Transaction> getAllTransactionsByAccount(@Param("account_id") Long id);
 }
